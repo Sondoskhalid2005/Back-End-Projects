5f3babe659f2800d052bf8ed88c8224e0f29ac6f
Sondoskhalid2005 <162904939+Sondoskhalid2005@users.noreply.github.com>
2025-03-19 09:55:54 +0200

    Delete src/Auth.Router.js

--- src/Auth.Router.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express")
-const router = Router()
-const controller = require("../controller/Auth.Controller");
-
-router.post("/signup", controller.signUp);
-router.post("/signin", controller.signIn);
-router.get("/signout", controller.signOut);
-
-module.exports = router ;
